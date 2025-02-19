dc32a3e09d2f1d265968efe8369c8154bd8fb2db
kazimi.msu@gmail.com <kazimi.msu@gmail.com>
2025-02-19 14:44:36 +0400

    change secure in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -33,7 +33,8 @@ export const login = async (req: Request, res: Response) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: false,
+            //secure: process.env.NODE_ENV === "production",
             sameSite: "strict"
         });
 
--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://157.230.29.19',
+    credentials: true
+}));
 app.use(express());
 app.use(cookieParser());
 
